de641e8cde90f91f879a6a14f7931943d917fdb9
Thomas Debrunner <thomas.debrunner@hotmail.com>
2022-04-13 14:52:04 +0200

    wip

--- main.py
+++ main.py
@@ -2,43 +2,72 @@ from state import create_initial_state
 import re
 import sys
 from termcolor import colored
-from modes import Takeoff
+from modes import create_modes
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
-def main_loop(state):
+
+def main_loop(vehicle):
     should_exit = False
+
     while not should_exit:
+        print(colored('User selected mode [%s], vehicle executing [%s]' %
+                      (vehicle.user_selected_mode, vehicle.executing_mode), 'blue'))
+
         command = input('> ')
 
         state_match = re.search('^state (.*)$', command)
         set_match = re.search('^set (.*) (ok|fail)$', command)
+        mode_match = re.search('^mode (.*)$', command)
 
         if command == 'state':
-            state.print()
+            vehicle.state.print()
+        elif command == 'modes':
+            print('Available modes:')
+            for mode in vehicle.modes:
+                print('- ' + mode)
         elif command == 'q' or command == 'exit':
             should_exit = True
         elif state_match:
             query = state_match.group(1)
             try:
-                state[query].print()
+                vehicle.state[query].print()
             except IndexError as err:
                 eprint(str(err))
         elif set_match:
             query = set_match.group(1)
             value = False if set_match.group(2) == 'fail' else True
             try:
-                state[query].set(value)
+                vehicle.state[query].set(value)
             except IndexError as err:
                 eprint(str(err))
+        elif mode_match:
+            mode = mode_match.group(1)
+            if mode in vehicle.modes:
+                vehicle.user_selected_mode = vehicle.modes[mode]
+            else:
+                eprint('%s is not a valid mode' % mode)
         elif not command == '':
             eprint('%s is not a command' % command)
 
 
+class Vehicle:
+    def __init__(self):
+        self.modes = create_modes()
+        self.state = create_initial_state()
+
+        self.user_selected_mode = self.modes['Idle']
+        self.executing_mode = self.modes['Idle']
+
+    def simulate(self):
+        pass
+
+
+
 def main():
-    state = create_initial_state()
-    main_loop(state)
+    vehicle = Vehicle()
+    main_loop(vehicle)
 
 
 
--- modes.py
+++ modes.py
@@ -1,8 +1,58 @@
+class CanRunResult:
+    def __init__(self, value, reason):
+        self.value = value
+        self.reason = reason
+
+    def __and__(self, other):
+        return CanRunResult(self.value and other.value, self.reason + other.reason)
+
+
 class Mode:
     pass
 
 
+class Idle(Mode):
+    name = 'Idle'
+
+    def can_run(self, state):
+        return not state['System/Armed'].ok()
+
+
 class Takeoff(Mode):
-    @staticmethod
-    def can_run(state):
+    name = 'Takeoff'
+
+    def can_run(self, state):
         return state['Hardware'].ok() and state['Estimator'].ok()
+
+
+class Loiter(Mode):
+    name = 'Loiter'
+
+
+class GoTo(Mode):
+    name = 'Goto'
+
+
+class RTL(Mode):
+    name = 'RTL'
+
+
+class MotorFailureLand(Mode):
+    name = 'MotorFailureLand'
+
+
+class Parachute(Mode):
+    name = 'Parachute'
+
+
+def create_modes():
+    modes = [
+        Idle(),
+        Takeoff(),
+        Loiter(),
+        GoTo(),
+        RTL(),
+        MotorFailureLand(),
+        Parachute()
+    ]
+    return {m.name: m for m in modes}
