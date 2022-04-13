042fcd028d70bfa18da5bbf69596e098bb2535a6
Thomas Debrunner <thomas.debrunner@hotmail.com>
2022-04-13 06:11:47 +0200

    added cli

--- main.py
+++ main.py
@@ -1,9 +1,45 @@
 from state import create_initial_state
+import re
+import sys
+from termcolor import colored
 from modes import Takeoff
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+def main_loop(state):
+    should_exit = False
+    while not should_exit:
+        command = input('> ')
+
+        state_match = re.search('^state (.*)$', command)
+        set_match = re.search('^set (.*) (ok|fail)$', command)
+
+        if command == 'state':
+            state.print()
+        elif command == 'q' or command == 'exit':
+            should_exit = True
+        elif state_match:
+            query = state_match.group(1)
+            try:
+                state[query].print()
+            except IndexError as err:
+                eprint(str(err))
+        elif set_match:
+            query = set_match.group(1)
+            value = False if set_match.group(2) == 'fail' else True
+            try:
+                state[query].set(value)
+            except IndexError as err:
+                eprint(str(err))
+        elif not command == '':
+            eprint('%s is not a command' % command)
+
 
 def main():
     state = create_initial_state()
-    state.print()
+    main_loop(state)
+
 
 
 if __name__ == '__main__':
--- state.py
+++ state.py
@@ -9,9 +9,15 @@ class StateItem:
     def ok(self):
         return self.value
 
+    def set(self, value):
+        self.value = value
+
     def print(self, level=0):
         state_str = colored('OK', 'green') if self.ok() else colored('Fail', 'red')
         print((' ' * level) + self.name + (' ' * (20-level-len(self.name))) + state_str)
+
+    def __getitem__(self, query):
+        raise IndexError('State item %s does not have any children %s' % (self.name, query))
 
 
 class StateGroup:
@@ -20,10 +26,22 @@ class StateGroup:
         self.children = children if children is not None else []
 
     def __getitem__(self, query):
+        next_item = query
+        remaining_query = ''
+        if '/' in query:
+            split = query.split('/')
+            next_item = split[0]
+            remaining_query = '/'.join(split[1:])
         for c in self.children:
-            if c.name == query:
-                return c
+            if c.name == next_item:
+                if remaining_query == '':
+                    return c
+                else:
+                    return c[remaining_query]
         raise IndexError('State %s does not exist' % query)
+
+    def set(self, value):
+        raise IndexError('State %s is a group, can not set directly' % self.name)
 
     def ok(self):
         for c in self.children:
