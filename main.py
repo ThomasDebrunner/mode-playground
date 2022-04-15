d9793a2ec9782bb438a05ee8c7c6c5527a4ee366
Thomas Debrunner <thomas.debrunner@hotmail.com>
2022-04-15 16:41:14 +0200

    Added fail safe stack logic

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from state import state_from_dict
 import re
 import sys
 from modes import modes_from_dict
-from colors import bold, blue
+from colors import bold, blue, red, yellow
 
 
-def eprint(*args, **kwargs):
-    print(*args, file=sys.stderr, **kwargs)
+def eprint(content):
+    print(red(content))
 
+def wprint(content):
+    print(yellow(content))
 
 def main_loop(vehicle):
     should_exit = False
@@ -26,21 +28,21 @@ def apply_command(vehicle, command):
     mode_match = re.search('^mode (.*)$', command)
 
     if command == 'state':
-        vehicle.state.print()
+        vehicle._state.print()
     elif command == 'modes':
         print('Available modes:')
-        for mode in vehicle.modes:
+        for mode in vehicle._modes:
             print('- ' + mode)
     elif command == 'stacks':
         print('Available stacks:')
-        for name, stack in vehicle.stacks.items():
+        for name, stack in vehicle._stacks.items():
             print('> ' + bold(name))
             for mode in stack:
                 print('  - ' + mode)
     elif state_match:
         query = state_match.group(1)
         try:
-            vehicle.state[query].print()
+            vehicle._state[query].print()
         except IndexError as err:
             eprint(str(err))
     elif set_match:
@@ -52,7 +54,7 @@ def apply_command(vehicle, command):
             eprint(str(err))
     elif mode_match:
         mode = mode_match.group(1)
-        if mode in vehicle.modes:
+        if mode in vehicle._modes:
             vehicle.request_mode(mode)
         else:
             eprint('%s is not a valid mode' % mode)
@@ -64,36 +66,73 @@ def apply_command(vehicle, command):
 
 class Vehicle:
     def __init__(self, modes, initial_state, stacks):
-        self.modes = modes
-        self.state = initial_state
-        self.stacks = stacks
+        self._modes = modes
+        self._state = initial_state
+        self._stacks = stacks
 
-        self._user_selected_mode = self.modes['Idle']
-        self._executing_mode = self.modes['Idle']
+        self._user_selected_mode = self._modes['Idle']
+        self._executing_mode = self._modes['Idle']
+        self._fail_safe_stack = None
 
         self._sim_time = 0
+        self._mode_request_reported = True
 
     def set_state(self, query, value):
-        self.state[query].set(value, self._sim_time)
+        self._state[query].set(value, self._sim_time)
 
     def request_mode(self, mode):
-        self._user_selected_mode = self.modes[mode]
+        self._user_selected_mode = self._modes[mode]
+        self._mode_request_reported = False
 
     def print_mode_state(self):
-        print(blue('Sim time: %d, user selected mode [%s], vehicle executing [%s]' %
-                      (self._sim_time, self._user_selected_mode.name, self._executing_mode.name)))
+        print(bold(blue('Sim time: %d, user selected mode [%s], vehicle executing [%s]' %
+                      (self._sim_time, self._user_selected_mode.name, self._executing_mode.name))))
+        if self._fail_safe_stack:
+            print(bold(red('Fail safe stack active: %s' % self._fail_safe_stack)))
 
     def simulate(self):
         self._sim_time += 1
 
-        res = self._user_selected_mode.can_run(self.state, self._sim_time)
-        if res.can_run:
-            self._executing_mode = self._user_selected_mode
-        else:
-            eprint('Can not execute %s because' % self._user_selected_mode.name)
+        # Check if the user requested mode can be respected
+        if self._user_selected_mode != self._executing_mode:
+            res = self._user_selected_mode.can_enter(self._state, self._sim_time) & \
+                  self._user_selected_mode.can_run(self._state, self._sim_time)
 
-        for reason in res.reasons:
-            eprint('- ' + reason)
+            if not res.can_run:
+                if not self._mode_request_reported:
+                    eprint('Can not switch to mode %s. Will switch as soon as condition allows' % self._user_selected_mode.name)
+                    eprint('Reasons:')
+                    for reason in res.reasons:
+                        eprint(' - ' + reason)
+                    self._mode_request_reported = True
+            else:
+                self._executing_mode = self._user_selected_mode
+                self._fail_safe_stack = None
+                print('Switched to mode %s' % self._user_selected_mode.name)
+
+        # Check if currently active mode can still run
+        res = self._executing_mode.can_run(self._state, self._sim_time)
+        if not res.can_run:
+            eprint('Can no longer execute mode %s, switching to fail safe %s stack...' %
+                   (self._executing_mode.name, res.fail_stack))
+            eprint('Reason:')
+            for reason in res.reasons:
+                eprint(' - ' + reason)
+
+            self._fail_safe_stack = res.fail_stack
+            for mode in self._stacks[self._fail_safe_stack]:
+                wprint('Attempting %s mode' % mode)
+                res = self._modes[mode].can_run(self._state, self._sim_time)
+                if res.can_run:
+                    wprint('Using %s mode as fail safe' % mode)
+                    self._executing_mode = self._modes[mode]
+                    break
+
+        else:
+            if len(res.reasons) > 0:
+                wprint('Warning:')
+                for reason in res.reasons:
+                    wprint(' - ' + reason)
 
 
 def main():
--- modes.py
+++ modes.py
@@ -1,13 +1,17 @@
 class CanRunResult:
-    def __init__(self, value, reasons):
+    def __init__(self, value, reasons, fail_stack):
         self.can_run = value
         self.reasons = reasons
+        self.fail_stack = fail_stack
 
     def __and__(self, other):
-        return CanRunResult(self.can_run and other.can_run, self.reasons + other.reasons)
+        return CanRunResult(self.can_run and other.can_run, self.reasons + other.reasons,
+                            self.fail_stack or other.fail_stack)
 
     def __or__(self, other):
-        return CanRunResult(self.can_run or other.can_run, [] if (self.can_run or other.can_run) else self.reasons + other.reasons)
+        return CanRunResult(self.can_run or other.can_run,
+                            [] if (self.can_run or other.can_run) else self.reasons + other.reasons,
+                            self.fail_stack or other.fail_stack)
 
 
 class Condition:
@@ -21,28 +25,35 @@ class Condition:
     def eval(self, state, time):
         state_val = state[self._query].ok()
         if state_val == self._expect:
-            return CanRunResult(True, [])
+            return CanRunResult(True, [], None)
         else:
             time_since_toggle = time - state[self._query].toggle_time()
             if time_since_toggle < self._hysteresis:
                 return CanRunResult(True, ['%s is in %s state since %d ago. Fail safe in %d' %
                                            (self._query, 'OK' if state_val else 'Fail', time_since_toggle,
-                                            self._hysteresis - time_since_toggle)])
+                                            self._hysteresis - time_since_toggle)], None)
             else:
-                return CanRunResult(False, ['%s is in %s state' % (self._query, 'OK' if state_val else 'Fail')])
+                return CanRunResult(False, ['%s is in %s state' % (self._query, 'OK' if state_val else 'Fail')],
+                                    self._fail_stack)
 
 
 class Mode:
-    def __init__(self, name, can_run_conditions):
+    def __init__(self, name, can_run_conditions, can_enter_conditions):
         self.name = name
         self._can_run_conditions = can_run_conditions
+        self._can_enter_conditions = can_enter_conditions
 
     def can_run(self, state, time):
-        res = CanRunResult(True, [])
+        res = CanRunResult(True, [], None)
         for condition in self._can_run_conditions:
             res = res & condition.eval(state, time)
         return res
 
+    def can_enter(self, state, time):
+        res = CanRunResult(True, [], None)
+        for condition in self._can_enter_conditions:
+            res = res & condition.eval(state, time)
+        return res
 
 def modes_from_dict(mode_dict):
     modes = {}
@@ -54,6 +65,13 @@ def modes_from_dict(mode_dict):
             hysteresis = condition_dict['hysteresis'] if 'hysteresis' in condition_dict else 0
             overridable = condition_dict['overridable'] if 'overridable' in condition_dict else False
             can_run_conditions.append(Condition(query, expect, fail_stack, hysteresis, overridable))
-        modes[name] = Mode(name, can_run_conditions)
+
+        can_enter_conditions = []
+        if 'can_enter' in conf:
+            for query, condition_dict in conf['can_enter'].items():
+                expect = condition_dict['expect']
+                # enter conditions can't failsafe, override or hysteresis
+                can_enter_conditions.append(Condition(query, expect, '', 0, False))
+        modes[name] = Mode(name, can_run_conditions, can_enter_conditions)
 
     return modes
